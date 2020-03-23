0905ccaa5e419bf43ba633ae8a5f2eff2a1f78d9
Sumeet Bansal <38594250+sumeetbansaloo7@users.noreply.github.com>
2020-03-23 22:52:32 +0530

    made final changes

--- final_program_using_mutex.c
+++ final_program_using_mutex.c
@@ -19,8 +19,8 @@ int *safe_sequence;
 
 //mutex lock variables
 pthread_mutex_t mutexlock;
-pthread_cond_t mutexcondition;
-int total_processes_completed = 0;
+pthread_cond_t mutexcondition;     // for making process execute in safe sequence this condion is implemented
+int total_processes_completed = 0; // this makes sure while threads run they get in to critical section in safe sequence
 
 //function for process that will be run by each new thread
 void *thread_function(void *);
@@ -31,6 +31,7 @@ int main()
 
     printf("\n Enter total no of resources:- ");
     scanf("%i", &type_of_resources);
+
     //r = row (no of process) c= column (type of processes)
     //this is done to make it easy to assign memory to matrixes
     int r = no_of_process;
@@ -49,54 +50,37 @@ int main()
         need[i] = (int *)malloc(c * sizeof(int));
 
     //end of memory allocation
-    int no_of_processz = 5;
-    int type_of_resourcesz = 3;
-    int avaliblez[3] = {3, 3, 2};
-    int currently_allocatedz[5][3] = {{0, 1, 0},  // P0    // Allocation Matrix
-                                      {2, 0, 0},  // P1
-                                      {3, 0, 2},  // P2
-                                      {2, 1, 1},  // P3
-                                      {0, 0, 2}}; // P4
-
-    int maxz[5][3] = {{7, 5, 3},  // P0    // MAX Matrix
-                      {3, 2, 2},  // P1
-                      {9, 0, 2},  // P2
-                      {2, 2, 2},  // P3
-                      {4, 3, 3}}; // P4
+    // marking all safe sequence positions -1
     for (int i = 0; i < no_of_process; i++)
     {
         safe_sequence[i] = -1;
-        printf("%d ", safe_sequence[i]);
     }
     //allocating values to arrays and matrixes
     for (int i = 0; i < c; i++)
     {
-        // printf("\n Enter Avalible instance of R%d", i);
-        avalible[i] = avaliblez[i]; //scanf("%i", &avalible[i]);
-        //printf("%i", avalible[i]);
+        printf("\n Enter Avalible instance of R%d:- ", i);
+        scanf("%i", &avalible[i]);
     }
     printf("\n Max required :- ");
     for (int i = 0; i < r; i++)
     {
-        //printf("\n FOR P%i :- ", i);
+        printf("\n FOR P%i :- ", i);
         for (int j = 0; j < c; j++)
         {
-            //printf("\n R%i:- ", j);
-            max[i][j] = maxz[i][j]; //scanf("%i", &max[i][j]);
+            printf("\n R%i:- ", j);
+            scanf("%i", &max[i][j]);
         }
     }
     printf("\n Currently Allocated required :- ");
     for (int i = 0; i < r; i++)
     {
-        //printf("\n FOR P%i :- ", i);
+        printf("\n FOR P%i :- ", i);
         for (int j = 0; j < c; j++)
         {
-            //printf("\n R%i:- ", j);
-            currently_allocated[i][j] = currently_allocatedz[i][j]; //scanf("%i", &currently_allocated[i][j]);
+            printf("\n R%i:- ", j);
+            scanf("%i", &currently_allocated[i][j]);
         }
     }
-    // making all safe sequence -1
-
     //calculating need matrix
     for (int i = 0; i < r; i++)
     {
@@ -105,6 +89,7 @@ int main()
             need[i][j] = max[i][j] - currently_allocated[i][j];
         }
     }
+    /*
     // testing if dynamically allocated memory worked for need matrix
     printf("\n \n");
     for (int i = 0; i < r; i++)
@@ -115,11 +100,30 @@ int main()
             printf("%d ", need[i][j]);
         }
         printf("\n");
+    }*/
+
+    //checking if max need of any process exceeds total avalible instances of a resource
+    for (int i = 0; i < type_of_resources; i++)
+    {
+        int total = avalible[i];
+        for (int j = 0; j < no_of_process; j++)
+        {
+            total += currently_allocated[j][i];
+        }
+        for (int j = 0; j < no_of_process; j++)
+        {
+            //if total instances of resource less than max required of any process
+            if (total < max[j][i])
+            {
+                printf("\n Max of P%d for Resource R%d is greater than total instances of Resource R%d", j, i, i);
+                return 0;
+            }
+        }
     }
     //banker's algorithm to find safe sequence and check if state is safe
     bool safe_sequence_not_found = true;
     //here we use a temporary avalible array as original one will be updated by threads
-    int t_avalible[type_of_resources]; // just to check for safe state
+    int t_avalible[type_of_resources]; // just to check for safe state this is tempory avalible array
     for (int i = 0; i < type_of_resources; i++)
     {
         t_avalible[i] = avalible[i];
@@ -131,11 +135,7 @@ int main()
         // means processes are not finished
         finished[i] = false;
     }
-    int count = 0; // putting processes into safe sequence
-    for (int i = 0; i < no_of_process; i++)
-    {
-        safe_sequence[i] = -1;
-    }
+    int count = 0;       // putting processes into safe sequence
     int current_process; //this variable keeps value of current process
     do
     {
@@ -196,13 +196,97 @@ int main()
             printf(" P%d -> ", safe_sequence[i]);
         else
             printf(" P%d", safe_sequence[i]);
-    } /*
-    for (int i = 0; i < type_of_resources; i++)
-    {
-        printf("\nR%d :- %d", i, avalible[i]);
     }
+    //all matrixes and avalible array before starting to make threads.
+    printf("\n\n Value Of All Matrixes Before Creating Multiple Threads :-  ");
+    printf("\n\n Avalible :- ");
     for (int i = 0; i < type_of_resources; i++)
     {
-        printf("\ntR%d :- %d", i, t_avalible[i]);
-    }*/
+        printf(" R%d :-  %d | ", i, avalible[i]);
+    }
+    printf("\n");
+    printf("\n Max matrix:\t\tAllocation matrix:\t\tNeed matrix:\n ");
+    for (int i = 0; i < no_of_process; i++)
+    {
+        for (int j = 0; j < type_of_resources; j++)
+            printf("%d  ", max[i][j]);
+        printf("\t\t");
+        for (int j = 0; j < type_of_resources; j++)
+            printf("%d  ", currently_allocated[i][j]);
+        printf("\t\t");
+        for (int j = 0; j < type_of_resources; j++)
+            printf("%d  ", need[i][j]);
+        printf("\n ");
+    }
+    printf("\n\n Creating and Executing %d Threads One for Each Process In Safe Sequence ", no_of_process);
+    pthread_t processes[no_of_process]; // threads ids
+    //pthread_attr_t attr;
+    //pthread_attr_init(&attr);
+    //creating threads
+    int process_numbers[no_of_process];
+    for (int i = 0; i < no_of_process; i++)
+    {
+        process_numbers[i] = i;
+    }
+    for (int i = 0; i < no_of_process; i++)
+    {
+        pthread_create(&processes[i], NULL, thread_function, &process_numbers[i]);
+    }
+    //waiting for all threads to finish
+    for (int i = 0; i < no_of_process; i++)
+    {
+        pthread_join(processes[i], NULL);
+    }
+    printf("\n All Programs Successefully Executed :) ");
+    sleep(4);
+}
+void *thread_function(void *args)
+{
+    //type casting and de refrencing the recieved parameter
+    int process_number = *((int *)args);
+    //printf("\n HELLO FROM P %d", process_number);//testing
+
+    pthread_mutex_lock(&mutexlock); // getting lock
+    //ensuring safe sequence by checking the condition
+    while (process_number != safe_sequence[total_processes_completed])
+        pthread_cond_wait(&mutexcondition, &mutexlock);
+    // code entering critical section
+    printf("\n\n PROCESS %d is being executed ", process_number);
+    sleep(1);
+    printf("\n Max required :- ");
+    for (int i = 0; i < type_of_resources; i++)
+    {
+        printf(" %d ", max[process_number][i]);
+    }
+    printf("\n Currently Allocated :- ");
+    for (int i = 0; i < type_of_resources; i++)
+    {
+        printf(" %d ", currently_allocated[process_number][i]);
+    }
+    printf("\n Needed :- ");
+    for (int i = 0; i < type_of_resources; i++)
+    {
+        printf(" %d ", need[process_number][i]);
+    }
+    printf("\n Avalible :- ");
+    for (int i = 0; i < type_of_resources; i++)
+    {
+        printf(" %d ", avalible[i]);
+    }
+    sleep(2);
+    printf("\n Resource Allocated to P%d", process_number);
+    //releasing resource
+    for (int i = 0; i < type_of_resources; i++)
+    {
+        avalible[i] += currently_allocated[process_number][i];
+    }
+    printf("\n Resource Released \n");
+    sleep(2);
+    //incrementing process completed
+    total_processes_completed++;
+
+    //releasing mutex lock and updating condition
+    pthread_cond_broadcast(&mutexcondition);
+    pthread_mutex_unlock(&mutexlock);
+    return NULL;
 }
